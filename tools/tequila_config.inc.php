5fb1144164c314e8c16e5822cb858bd5a4e7ab16
strikerpt <root@itsidevfsd0008.xaas.epfl.ch>
2020-10-07 16:25:39 +0200

    Fichier de configuration de tequila.php.

---
+++ tools/tequila_config.inc.php
@@ -0,0 +1,19 @@
+<?php
+$aConfig = array (
+	          'sServer' => 'https://tequila.epfl.ch',
+	          'iTimeout' => 86400,
+			  'logoutUrl' => "http://itsidevfsd0008.xaas.epfl.ch/",
+	);
+
+
+/********************************************************
+          DO NOT EDIT UNDER THIS LINE
+********************************************************/
+function GetConfigOption($sOption, $sDefault = '') {
+  global $aConfig;
+  if (!array_key_exists ($sOption, $aConfig))
+    return ($sDefault);
+  else
+    return ($aConfig [$sOption]);
+}
+?>
