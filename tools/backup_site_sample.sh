910588c4faa4ca2c4bfae51b24177e5cc6671a43
strikerpt <hugo.valente@epfl.ch>
2020-10-13 10:27:54 +0200

    template du script de backup sans le mot de passe ni l'user

---
+++ tools/backup_site_sample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Backup des répertoires du site "projet_startups/"
+
+#Extraction du backup sur linux
+#tar -zxvf  ~/backup_site/backup_site__(date).tar.gz
+
+#Récupérer le backup de la base de données
+#mysql -u vpi_startup_mgmt -h mysql-scx.epfl.ch -P 33001 -p < backup_db_(date).sql
+
+#Mettre dans une variable la date du jour
+today=$(date +"%d_%m_%Y")
+
+#Compresser les fichiers du site et mettre la date du jour dans le nom du tar
+tar -zcvf ~/backup_site/backup_site__${today}.tar.gz /var/www/html/projet_startups/
+
+#Créer un dump de la base de données et placer le fichier dans le répertoire backup_site/
+mysqldump --user="database_user" --password="database_password" --databases vpi_startup -h mysql-scx.epfl.ch -P 33001 > ~/backup_site/backup_db_${today}.sql
