56c8e22ea8e8b72076db681cfac797a1d9eee38a
strikerpt <hugo.valente1996@gmail.com>
2021-04-07 15:48:45 +0200

    Utiliser les variables d'environnement pour la connection à la db

--- tools/connection_db.php
+++ tools/connection_db.php
@@ -1,24 +1,19 @@
 <?php
 
-//Traitement du fichier qui contient le mot de passe de la base de données pour qu'il puisse être utilisé
-$dir = '/var/database_file';
-$myfile = fopen($dir, "r");
-$database_file = fread($myfile,filesize($dir));
-$database_file2 = preg_replace('/\s/', '', $database_file);
+require 'env.php';
 
 //Permet de faire se connecter à la base de données pour faire les requêtes
-$db_name="vpi_startup";
-$servername = "mysql-scx.epfl.ch";
-$username = "vpi_startup_mgmt";
-$port = 33001;
-
-
+$db_name=getenv("DB_NAME");
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USERNAME");
+$port = getenv("DB_PORT");
+$password = getenv("DB_PASSWORD");
 
 
 //Si la connexion n'est pas réussie, alors il affiche un message de connection failed
 try 
 {
-  $db = new PDO("mysql:host=$servername;port=$port;dbname=$db_name;charset=utf8", $username, $database_file2);
+  $db = new PDO("mysql:host=$servername;port=$port;dbname=$db_name;charset=utf8", $username, $password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } 
 catch(PDOException $e) 
